188ef6583deec2190ccbdf07107bf101a5e2c394
sharablack81-eng <sharablack81@gmail.com>
2026-01-15 21:45:25 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -93,6 +93,15 @@ const App = () => {
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
+     {/* Legal Footer */}
+      <footer style={{ marginTop: '60px', padding: '20px', textAlign: 'center', borderTop: '1px solid #e5e5e5' }}>
+        <Link to="/PrivacyPolicy" style={{ margin: '0 15px', textDecoration: 'underline' }}>
+          Privacy Policy
+        </Link>
+        <Link to="/TermsOfService" style={{ margin: '0 15px', textDecoration: 'underline' }}>
+          Terms of Service
+        </Link>
+      </footer>
   );
 };
 
