b9b5e488d95dd84d75ba13d17d8224f7fffc20c5
sharablack81-eng <sharablack81@gmail.com>
2026-01-16 09:15:39 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,7 @@ const App = () => {
           Terms of Service
         </Link>
       </footer> 
-      </>
+        </Routes>
           {/* Routes configured above */}
           <Route path="*" element={<NotFound />} />
         </Routes>
